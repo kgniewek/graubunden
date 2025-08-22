984ac9224e916b14d417047dc333a1304314e412
Krzysztof Gniewek <126593238+kgniewek@users.noreply.github.com>
2025-08-22 18:17:23 +0200

    Update client-layout.tsx

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -115,8 +115,8 @@ export function ClientLayout() {
         className="h-8 px-2"
         onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
       >
-        <Sun className="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <Moon className="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Sun className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <Moon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
         <span className="sr-only">
           <T
             en="Toggle theme"
